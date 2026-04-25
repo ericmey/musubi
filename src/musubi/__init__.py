4f569ae1c9f41ba86b7fa65018f9c470a48d4d3b
Eric Mey <ericmey@gmail.com>
2026-04-25 06:56:10 -0400

    chore(main): release 1.2.2 (#278)

--- src/musubi/__init__.py
+++ src/musubi/__init__.py
@@ -4,4 +4,4 @@ See the architecture vault for design. Code is sliced: each public subpackage
 realises one slice from ``_slices/`` in the vault, one Test Contract at a time.
 """
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
